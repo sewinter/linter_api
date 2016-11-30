8a3fc991890741b823928eec39df63fe2b854696
sewinter <samuelewinter@gmail.com>
2016-11-29 20:44:33 -0800

    add statistical speller

--- linter/views.py
+++ linter/views.py
@@ -4,28 +4,31 @@ from django.http import JsonResponse
 import spacy.en
 import spell
 import re
-import pdb
 
-nlp = spacy.en.English()
+# nlp = spacy.en.English()
 
 def words_with_indices(string):
-    l = re.split("(\s|\W){1}", string)
-    counter = 0
-    w_i = []
-    for tok in l:
-        if re.match('[a-zA-Z]', tok):
-            w_i.append([tok, counter])
-        counter += len(tok)
-    return w_i
+   l = re.split("(\s|\W){1}", string)
+   counter = 0
+   w_i = []
+   for tok in l:
+       if re.match('[a-zA-Z]', tok):
+           w_i.append([tok, counter])
+       counter += len(tok)
+   return w_i
 
 class LintView(TemplateView):
-    def get(self, request, **kwargs):
-        try:
-            text = request.GET.urlencode().decode('utf-8').replace('_', ' ')
-            doc = nlp(text)
-            for tok in doc:
-                print tok.orth_, tok.pos_
-
-            return JsonResponse(doc[0].orth_, safe=False)
-        except Exception, e:
-            return JsonResponse(str(e), safe=False)
+   def get(self, request, **kwargs):
+       spellings = {}
+       try:
+           text = request.GET.urlencode().decode('utf-8').replace('_', ' ')
+        #    doc = nlp(text)
+        #    for tok in doc:
+        #        print tok.orth_, tok.pos_
+           for word, idx in words_with_indices(text):
+               correction = spell.correction(word)
+               if word.lower() != correction.lower():
+                   spellings[idx] = correction
+           return JsonResponse(spellings)
+       except Exception, e:
+           return JsonResponse(str(e), safe=False)
